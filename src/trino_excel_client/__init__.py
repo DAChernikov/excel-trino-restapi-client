806644b44a5a4ac3525fcc7b7ea73f319981642a
DAChernikov <129949710+DAChernikov@users.noreply.github.com>
2026-06-03 10:11:23 +0300

    update version

--- src/trino_excel_client/__init__.py
+++ src/trino_excel_client/__init__.py
@@ -1,7 +1,7 @@
 from .excel_com import create_advanced_workbook, create_workbook, install_advanced_client, install_client
 from .openpyxl_backend import create_advanced_workbook_ui, create_workbook_ui, install_advanced_client_ui_file, install_client_ui_file
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "__version__",
